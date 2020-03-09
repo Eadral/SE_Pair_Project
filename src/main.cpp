97e709fa24bb2a94f6e0ad1ea608ce4b38d65db1
Eadral <a250014238@me.com>
2020-03-10 00:15:46 +0800

    :sparkles: Add a better arg handling
    Support real CLI argument handling.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,22 @@
 
 int main(int argc, char *argv[]) {
     std::ios_base::sync_with_stdio(false);
+
     if (argv == nullptr)
         return -1;
-    // TODO(zyc): better args handling
-    const char* input_filename = argv[2];
-    const char* output_filename = argv[4];
+
+    std::string input_filename;
+    std::string output_filename;
+    for (int i = 0; i < argc-1; i++) {
+        if (strcmp(argv[i], "-i") == 0)
+            input_filename = std::string(argv[i + 1]);
+        if (strcmp(argv[i], "-o") == 0)
+            output_filename = std::string(argv[i + 1]);
+    }
+    if (input_filename.empty() || output_filename.empty()) {
+        std::cout << "Usage: intersect.exe -i input.txt -o output.txt" << std::endl;
+        return -1;
+    }
 
     std::ifstream fin(input_filename);
     std::ofstream fout(output_filename);
